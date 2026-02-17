7876d6e17aba3e00fe445a8a329da015ea5a7c28
Newsha R <>
2026-02-17 15:47:03 -0800

    Adding code

--- Class-1/assignments/assignment_5.py
+++ Class-1/assignments/assignment_5.py
@@ -6,3 +6,22 @@ Assignment 5: Dictionaries
 3.  Update the "year_published" to a new value and print the dictionary again.
 4.  Add a new key-value pair for "genre" and print the final dictionary.
 """
+
+print("\n== Assignment 5: Dictionaries == ") 
+
+book1= {"Title": "The Hare Who Wouldn't Share",  "Author" :"Steve Small" , "year_published": "2024"} # Creating a Dictionary
+
+print("1. Dictionary Creation")
+
+print(f" Book 1: {book1}")    # Print the dictionary
+
+book1["year_published"] = "2025"  # Modifying exsiting
+print(f" Book 1: {book1}")
+
+print("\n== Modifying Dictionary")
+book1["Genre"]= "Kids Book for Adults"  # Add a new Key-value "Genre"
+print(f"Final Dictionary: {book1}")
+
+
+
+
