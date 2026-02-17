5cf1651819701e2e44cb2c626046bcf7e002162e
Newsha R <>
2026-02-16 21:04:38 -0800

    Fix arithmetic operation and enhance print statements for clarity

--- Class-1/Assignment-operator.py
+++ Class-1/Assignment-operator.py
@@ -4,10 +4,23 @@ c = a % b
 print(c)
 
 print("before value of a is ", str(a))  # 10
-a += 2  # 10 + 1  = a (11)
+a += 2  # 10 + 2  = a (12)
 # a = a + 1
 print("after value of a is ", str(a))  # 20
-# print(a)
 
+# print(a)
+print("/--n Newsha practices here!")
 # a -= 10
 # print(a)
+
+a = 12
+b = 3
+c = a % b
+print(c)
+
+print("before value of a is" , str(a)) 
+a += 6 # 10 + 2 = a (12)
+
+print("after value of a is", str(a))
+
+
