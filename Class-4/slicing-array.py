5204afba12407e940497e218a81acc4da22c9b5e
Newsha R <>
2026-02-16 21:03:04 -0800

    Adding comments inline throughout

--- Class-4/slicing-array.py
+++ Class-4/slicing-array.py
@@ -11,11 +11,11 @@ print(a[-5:-2])
 print(a[:3])
 
 a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(a[0, 1:4])
-print(a[0:2, 2])
-print(a[0:2, 1:4])
+print(a[0, 1:4])  # Frist row, 2nd to 4th column
+print(a[0:2, 2])  # First two rows, 3rd column
+print(a[0:2, 1:4]) # First two row, 2nd to 4th colum
 
-a = np.arange(125).reshape(25, 5)
+a = np.arange(25).reshape( 5, 5)
 print(a)
 
 print(a[1, -1])  # we can count backwards
