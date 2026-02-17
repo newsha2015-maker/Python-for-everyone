8bc2acfc2004438bb53d6f44cb748e61abcb90c7
Newsha R <>
2026-02-17 12:46:13 -0800

    Addes my code + Comments

--- Class-1/assignments/assignment_2.py
+++ Class-1/assignments/assignment_2.py
@@ -5,3 +5,11 @@ Assignment 2: User Input and String Formatting
 2.  Store the user's input in two separate variables.
 3.  Print a message to the user that includes their name and favorite number. Use an f-string for formatting. For example: "Hello [name], your favorite number is [number]."
 """
+
+print("\n== Assignment 2: User Input and String Formatting ==")
+
+name = input("What is your name?")        # Storing user input into a variable name
+number = input("your favorite number?")   # Sotrying user imput into a variable number
+
+print(f"Hello, My name is {name} and my favorite number is {number}.")  
+
