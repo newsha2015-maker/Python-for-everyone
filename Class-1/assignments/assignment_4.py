97e03728c1650857150ba535d2d9ffe9256f1ad0
Newsha R <>
2026-02-17 16:31:50 -0800

    Stuck on "while loop"

--- Class-1/assignments/assignment_4.py
+++ Class-1/assignments/assignment_4.py
@@ -6,3 +6,22 @@ Assignment 4: Loops and Lists
 3.  Create a second list of numbers from 1 to 10.
 4.  Use a `while` loop to print the square of each number in the list.
 """
+
+print("\n== Assignment 4: Loops and Lists")
+
+fruit = ["Grapes", "Orange", "Watermelon", "Lechi", "Berries", "Peach"]  # Create a list of fruits
+# print(f"{fruit}") #Testing
+
+for fruit in fruit:  # for loop
+    print(fruit)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] # Create a list from 1 to 10 
+#print(f"{numbers}") #Testing
+for numbers in numbers:
+    print(numbers)
+
+while count < 9:
+    count = (numbers[])
+    squred_number = numbers ** 2   # while loop
+    print(squred_number)
+    count = count + 1
