e36c89ed4791203e65351f5c54cc70f72e74e0d2
Newsha R <>
2026-02-17 17:11:49 -0800

    Adding Code

--- Class-1/assignments/assignment_3.py
+++ Class-1/assignments/assignment_3.py
@@ -7,3 +7,14 @@ Assignment 3: Conditional Logic
 4.  If the number is less than 50, print "The number is small."
 5.  Include a check for the number being exactly 50 or 100.
 """
+print("\n == Assignment 3: Conditional Logic ==")
+
+number = int(input(f"Enter a number:"))   # Ask the user for a number
+if number > 100: 
+    print(f"The number is large.")
+elif number >= 50 and  number <= 100:     # Check if number is between 50 and 100
+    print(f"The number is medium.")
+elif number < 50:
+    print(f"Number is small.")            # Check if number is small 
+else:
+     print(f"The number is in fact bewteen 50 and 100")
